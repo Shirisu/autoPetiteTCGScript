bcc87ef3b3a98a9ddf8cc51c5b310621038f464c
Shirisu <ebilpain@googlemail.com>
2021-03-13 15:48:30 +0100

    adjust constants for documentation

--- inc/constants.php
+++ inc/constants.php
@@ -18,20 +18,19 @@ define('TCG_META_KEYWORDS', 'TCG, tcg, tgc, trading card game, trade card game, 
 define('TCG_META_OWNER', 'mail@host.com'); // meta owner
 define('TCG_META_AUTHOR', 'Admin'); // meta author
 define('TCG_MAIN_LANGUAGE', 'en'); // main language (en or de)
-define('TCG_CARDDECK_MAX_CARDS', '12'); // max cards of carddecks
+define('TCG_CARDDECK_MAX_CARDS', 12); // max cards of carddecks
 define('TCG_CARDS_FILE_TYPE', 'png'); // file type for cards (allowed types: gif, jpg, png - use only one!)
-define('TCG_CARDS_WIDTH', '115'); // width of card
-define('TCG_CARDS_HEIGHT', '95'); // height of card
-define('TCG_CARDS_PER_ROW', '4'); // how many cards are in one row
-define('TCG_MASTERCARDS_WIDTH', '153'); // width of master
-define('TCG_MASTERCARDS_HEIGHT', '123'); // height of master
-define('TCG_CARDS_START_PACKAGE', '12'); // cards of startdeck
+define('TCG_CARDS_WIDTH', 100); // width of card
+define('TCG_CARDS_HEIGHT', 100); // height of card
+define('TCG_CARDS_PER_ROW', 4); // how many cards are in one row
+define('TCG_MASTERCARDS_WIDTH', 100); // width of master
+define('TCG_MASTERCARDS_HEIGHT', 100); // height of master
+define('TCG_CARDS_START_PACKAGE', 12); // cards of startdeck
 define('TCG_USE_CURRENCY', true); // will you use the currency? (yes = true - no = false)
 define('TCG_CURRENCY', 'Dollar'); // currency name
 define('TCG_USE_WISH', true); // will you use wish? (yes = true - no = false)
 define('TCG_WISH', 'Wish'); // wish name
-define('TCG_MASTER_CARD_REWARD', '3'); // reward card amount for mastering
-define('TCG_LEVEL_UP_CARD_REWARD', '3'); // reward card amount for level up
-define('TCG_MEMBER_MAX_LVL', '20'); // max level for member
-define('TCG_SHOP_CURRENCY_FOR_RANDOM', '100'); // how much currency do 1 random cost?
+define('TCG_MASTER_CARD_REWARD', 3); // reward card amount for mastering
+define('TCG_LEVEL_UP_CARD_REWARD', 3); // reward card amount for level up
+define('TCG_SHOP_CURRENCY_FOR_RANDOM', 100); // how much currency do 1 random cost?
 ?>
