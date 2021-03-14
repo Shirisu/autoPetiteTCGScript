ee9129dd9f635f14be18cb16046341826b45228f
Shirisu <ebilpain@googlemail.com>
2021-03-14 22:07:54 +0100

    fixed small bug in constants.php - wrong order for some constants

--- inc/constants.php
+++ inc/constants.php
@@ -9,6 +9,10 @@ define('MYSQL_VERSION', mysqli_get_client_info()); // DO NOT EDIT THIS LINE
 define('TCG_CARDS_FOLDER', '/assets/cards'); // DO NOT EDIT THIS LINE
 
 // BELOW THIS YOU CAN EDIT THE VALUES - BUT BE CAREFUL!
+define('TCG_NAME', 'autoPetiteTCGScript'); // for title-attribute & meta title
+define('TCG_SLOGAN', 'a simple mini Trading Card Game'); // for title-attribute & meta title
+define('TCG_META_TITLE', TCG_NAME .' :: '.TCG_SLOGAN); // title-tag & meta title
+
 define('TCG_CARDS_FILE_TYPE', 'png'); // file type for cards (allowed types: gif, jpg, png - use only one!)
 define('TCG_CARDS_FILLER_NAME', 'filler'); // name of the filler/search card - should be placed in the the folder "/assets/cards/"
 define('TCG_CARDS_HEIGHT', 100); // height of card
@@ -26,11 +30,8 @@ define('TCG_MASTERCARDS_WIDTH', 100); // width of master
 define('TCG_META_AUTHOR', 'Admin'); // meta author
 define('TCG_META_DESC', 'a mini TCG'); // meta description
 define('TCG_META_KEYWORDS', 'TCG, tcg, tgc, trading card game, trade card game, trade, card, game, ccg, collectible card game, virtual card game, vcg'); // meta keywords
-define('TCG_META_TITLE', TCG_NAME .' :: '.TCG_SLOGAN); // title-tag & meta title
 define('TCG_META_OWNER', 'mail@host.com'); // meta owner
-define('TCG_NAME', 'autoPetiteTCGScript'); // for title-attribute & meta title
 define('TCG_SHOP_CURRENCY_FOR_RANDOM', 100); // how much currency do 1 random cost?
-define('TCG_SLOGAN', 'a simple mini Trading Card Game'); // for title-attribute & meta title
 define('TCG_WISH', 'Wish'); // wish name
 define('TCG_WISH_USE', true); // will you use wish? (yes = true - no = false)
 ?>
