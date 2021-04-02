be5336f5a14d2bd4fcccae5cd22742f60407f937
Shirisu <ebilpain@googlemail.com>
2021-04-02 11:24:46 +0200

    bugfix - order receiver names alphabetically on write message page

--- tcg/message/message_write.php
+++ tcg/message/message_write.php
@@ -138,7 +138,8 @@ if (isset($_SESSION['member_rank'])) {
             $sql_member = "SELECT member_id, member_nick
                        FROM member
                        WHERE member_active = 1
-                         AND member_id != '" . $member_id . "'";
+                         AND member_id != '" . $member_id . "'
+                       ORDER BY member_nick ASC";
             $result_member = mysqli_query($link, $sql_member) OR die(mysqli_error($link));
             ?>
             <form action="<?php echo HOST_URL; ?>/message/write" method="post">
